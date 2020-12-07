0686b381ed04a095838affa02fd7aa694df1a68f
Your Name <gabriel.cavicchioli@turingbits.com.br>
2020-12-07 20:20:30 -0300

    wip

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ module.exports = async () => {
     mongoDbConnection()
     fastify.register(require('fastify-jwt'), { secret: process.env.API_SECRET })
     fastify.register(fastifyCors, {})
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
