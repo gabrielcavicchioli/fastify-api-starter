82bb8f708f0c1e52d83a5a6fd691fa6c72bfcdd0
Your Name <gabriel.cavicchioli@turingbits.com.br>
2020-12-08 08:38:39 -0300

    wip

--- server/registerRoutes.js
+++ server/registerRoutes.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = (fastify) => {
   fastify.register((instance, opts, next) => {
-    fastify.get('/', (request, reply) => reply.send('API IS RUNNING!!!'))
+    fastify.get('/', (request, reply) => reply.send('API IS RUNNING!!!')) 
     readdirSync(path.join(__dirname, '../routes')).forEach(folderName => {
       const fullPath = path.join(__dirname, '../routes', folderName)
       const module = require(fullPath)
